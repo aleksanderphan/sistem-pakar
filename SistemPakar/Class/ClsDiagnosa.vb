1e4d596e9aee6a091c67737d59882b8ee037bf01
aleksanderphan <92428843+aleksanderphan@users.noreply.github.com>
2022-10-20 14:58:54 +0700

    Delete SistemPakar directory

--- SistemPakar/Class/ClsDiagnosa.vb
+++
@@ -1,3 +0,0 @@
-﻿Public Class ClsDiagnosa
-
-End Class
--- SistemPakar/Class/ModConnection.vb
+++
@@ -1,7 +0,0 @@
-﻿Imports System.Data.OleDb
-
-Module ModConnection
-    Dim conn As OleDbConnection = New OleDbConnection("Connection String Here")
-    Dim cmd As New OleDbCommand
-
-End Module
