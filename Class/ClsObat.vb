c666379d9acab8a73a5deaef4c1d931dc8a61990
aleksanderphan <92428843+aleksanderphan@users.noreply.github.com>
2022-10-20 15:02:41 +0700

    Add files via upload

---
+++ Class/ClsObat.vb
@@ -0,0 +1,10 @@
+﻿Imports System.Data.OleDb
+
+Public Class ClsObat
+
+    Public Sub LoadDB(dgv As DataGridView)
+        dt = LoadTable("SELECT [Kode], [Nama], [Harga] FROM [Obat] ORDER BY [ID] ASC")
+        dgv.DataSource = dt
+    End Sub
+
+End Class
---
+++ Form/FrmDiagnosa.vb
@@ -0,0 +1,16 @@
+﻿Public Class FrmDiagnosa
+
+    Dim d As New ClsDiagnosa
+
+    Private Sub FrmDiagnosa_Load(sender As Object, e As EventArgs) Handles MyBase.Load
+        d.ShowGejala(rtxtGejala)
+    End Sub
+
+    Private Sub BtnYa_Click(sender As Object, e As EventArgs) Handles btnYa.Click
+        d.GejalaYa(rtxtGejala)
+    End Sub
+
+    Private Sub BtnTidak_Click(sender As Object, e As EventArgs) Handles btnTidak.Click
+        d.GejalaTidak(rtxtGejala)
+    End Sub
+End Class
